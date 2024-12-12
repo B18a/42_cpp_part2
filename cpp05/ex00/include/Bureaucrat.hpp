16819af7e66738d1a8f291e22dbb4cd7f414900b
B18a <andreasjehle89@web.de>
2024-12-12 13:40:25 +0100

    Bureaucrat start

---
+++ cpp05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,26 @@
+
+
+# include <iostream>
+
+
+#define HIGHEST_GRADE 1
+#define LOWEST_GRADE 150
+
+class Bureaucrat
+{
+	public:
+
+
+
+	private:
+	const	std:string _name;
+	int		_grade;
+
+
+}
+
+
+
+
+// Bureaucrat::GradeTooHighException
+// Bureaucrat::GradeTooLowException
