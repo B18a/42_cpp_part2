aa03cbb385efb3628751e6a071415cb62f29acfd
BiBa <andreasjehle89@web.de>
2025-01-09 14:31:19 +0100

    start of ex01

--- cpp06/ex00/src/main.cpp
+++ cpp06/ex00/src/main.cpp
@@ -5,18 +5,8 @@ int main(int argc, char** argv)
 {
 
 	(void)argc;
-	// std::cout << argv[1] << std::endl;
 	if(argc == 2)
 		ScalarConverter::convert(argv[1]);
 	return 0;
 }
 
-
-
-// 1. get (Literal)Type
-// 2. convert string to Literal Type
-// 3. print string in all Literal Types
-// 		- char
-// 		- int
-// 		- float
-// 		- double
