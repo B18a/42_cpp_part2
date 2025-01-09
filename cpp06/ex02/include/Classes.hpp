e0210f2f4360c2ea94e1aaf7bfeef0ccf1495363
BiBa <andreasjehle89@web.de>
2025-01-09 17:45:44 +0100

    classes made

---
+++ cpp06/ex02/include/Classes.hpp
@@ -0,0 +1,55 @@
+
+// #include<iostream>
+
+#pragma once
+
+/****************************************/
+/*				Class A					*/
+/****************************************/
+#ifndef A_HPP
+#define A_HPP
+
+class A : public Base{};
+
+#endif
+
+/****************************************/
+/*				Class B					*/
+/****************************************/
+#ifndef B_HPP
+#define B_HPP
+
+class B : public Base{};
+
+#endif
+
+/****************************************/
+/*				Class C					*/
+/****************************************/
+#ifndef C_HPP
+#define C_HPP
+
+class C : public Base{};
+
+#endif
+
+/****************************************/
+/*				Class Base				*/
+/****************************************/
+#ifndef BASE_HPP
+#define BASE_HPP
+
+
+class Base
+{
+	private:
+
+	public:
+		// virtual ~Base(void);
+
+		Base*	generate(void); // It randomly instanciates A, B or C and returns the instance as a Base pointer. Feel free to use anything you like for the random choice implementation.
+		void	identify(Base* p); //It prints the actual type of the object pointed to by p: "A", "B" or "C".
+		void	identify(Base& p); //It prints the actual type of the object pointed to by p: "A", "B" or "C". Using a pointer inside this function is forbidden
+};
+
+#endif
---
+++ cpp06/ex02/src/Classes.cpp
@@ -0,0 +1,15 @@
+
+#include"../include/Classes.hpp"
+
+Base*	Base::generate(void)
+{
+
+}
+void	Base::identify(Base* p)
+{
+
+}
+void	Base::identify(Base& p)
+{
+	
+}
