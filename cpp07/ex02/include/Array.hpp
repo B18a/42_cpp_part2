346eaedb6b494ebbbf00fc1645fac25c3a2f94f3
B18a <andreasjehle89@web.de>
2025-01-19 10:38:21 +0100

    removed array.tpp

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -10,17 +10,47 @@ template <typename T>
 class Array
 {
 	private:
-		T*				element;
-		unsigned int	len;
+		T*				_element;
+		unsigned int	_len;
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(const Array& other);
-		Array& operator=(const Array& other);
-		~Array();
+		Array(){};
+		Array(unsigned int n) : _len(n), _element = new T[n]();
+		Array(const Array& other) : _len(other._len), _element(new T[other._len]())
+		{
+			for(int i = 0; i < _len; i++)
+			{
+				_element[i] = other._element[i];
+			}
+		}
+		Array& operator=(const Array& other)
+		{
+			if(this != other)
+			{
+				delete[] _element;
+				_len = other._len;
+				_element = new T[other._len]();
+				for(int i = 0; i < _len; i++)
+				{
+					_element[i] = other._element[i];
+				}
+			}
+			return *this;
+		}
+		~Array()
+		{
+			delete[] this->_element;
+		}
 
-		T& operator[](int pos);
-		int	size(void);
+		T& operator[](int pos)
+		{
+			if(pos > this._len)
+				throw std::out_of_range("Index out of range");
+			return _element[pos];
+		}
+		int	size(void)
+		{
+			return _len;
+		}
 
 };
 
@@ -28,34 +58,59 @@ class Array
 
 
 
-template <typename T>
-class Math
-{
-	private:
-		T first;
-		T second;
+// Allocating Memory using () initializes the value to 0
+/*
+    int*	i = new int();			// Default-initialized to 0
+    float*	f = new float();		// Default-initialized to 0.0
+    double*	d = new double();		// Default-initialized to 0.0
+    char*	c = new char();			// Default-initialized to '\0'
 
-	public:
-		Math(){	};			// Default Constructor
-		Math(T a, T b)		// Constructor with Parameters
-		{
-			first = a;
-			second = b;
-		};
-		void addnumbers();
-};
+    int*	i_uninit = new int;		// Uninitialized (garbage value)
+    float*	f_uninit = new float;	// Uninitialized (garbage value)
+    double*	d_uninit = new double;	// Uninitialized (garbage value)
+    char*	c_uninit = new char;	// Uninitialized (garbage value)
 
 
+// std::string is a class type with constructors that handle initialization:
 
+    std::string* s1 = new std::string();  // Default-initialized to ""
+    std::string* s2 = new std::string;   // Also default-initialized to ""
 
+*/
+
+/*********************************/
+/*	Example of Template Class 	*/
+/*********************************/
+
+// template <typename T>
+// class Math
+// {
+// 	private:
+// 		T first;
+// 		T second;
+
+// 	public:
+// 		Math(){	};			// Default Constructor
+// 		Math(T a, T b)		// Constructor with Parameters
+// 		{
+// 			first = a;
+// 			second = b;
+// 		};
+// 		void addnumbers();
+// };
 
 
 /********************************************************/
 /*	whenever a function is defined outside of a class 	*/
 /*	the template statement has to be written again 		*/
 /********************************************************/
-template <typename T>
-void Math<T>::addnumbers()
-{
-	std::cout<< this->first << " " << this->second << std::endl;
-}
+// template <typename T>
+// void Math<T>::addnumbers()
+// {
+// 	std::cout<< this->first << " " << this->second << std::endl;
+// }
+
+// template <typename T>
+// inline Array<T>::Array()
+// {
+// }
--- cpp07/ex02/src/Array.tpp
+++ cpp07/ex02/src/Array.tpp
@@ -5,84 +5,64 @@
 #include <exception>
 
 
-template <typename T>
-Array<T>::Array()
-{
-	this->element = nullptr;
-};
+// template <class T>
+// Array<T>::Array()
+// {
+// 	this->_element = nullptr;
+// };
 
 
-template <typename T>
-Array<T>::Array(unsigned int n)
-{
-	this->element = new T[n]();
-};
+// template <typename T>
+// Array<T>::Array(unsigned int n)
+// {
+// 	this->_element = new T[n]();
+// };
 
 
-template <typename T>
-Array<T>::Array(const Array& other) : len(other.len), element(new T[other.len]())
-{
-	for(int i = 0; i < len; i++)
-	{
-		element[i] = other.element[i];
-	}
-}
+// template <typename T>
+// Array<T>::Array(const Array& other) : _len(other._len), _element(new T[other._len]())
+// {
+// 	for(int i = 0; i < _len; i++)
+// 	{
+// 		_element[i] = other._element[i];
+// 	}
+// }
 
-template <typename T>
-Array<T>& Array<T>::operator=(const Array& other)
-{
-	if(this != other)
-	{
-		delete[] element;
-		len = other.len;
-		element = new T[len]();
-		for(int i = 0; i < len; i++)
-		{
-			element[i] = other.element[i];
-		}
-	}
-	return *this;
-}
+// template <typename T>
+// Array<T>& Array<T>::operator=(const Array& other)
+// {
+// 	if(this != other)
+// 	{
+// 		delete[] _element;
+// 		_len = other._len;
+// 		_element = new T[other._len]();
+// 		for(int i = 0; i < _len; i++)
+// 		{
+// 			_element[i] = other._element[i];
+// 		}
+// 	}
+// 	return *this;
+// }
 
-template <typename T>
-Array<T>::~Array()
-{
-	delete[] this->element;
-};
+// template <typename T>
+// Array<T>::~Array()
+// {
+// 	delete[] this->_element;
+// };
 
-template <typename T>
-T& Array<T>::operator[](int pos)
-{
-	if(pos > this.len)
-		throw std::out_of_range("Index out of range");
-	return element[pos];
-}
-
-
-template <typename T>
-int Array<T>::size(void)
-{
-	return len;
-}
+// template <typename T>
+// T& Array<T>::operator[](int pos)
+// {
+// 	if(pos > this._len)
+// 		throw std::out_of_range("Index out of range");
+// 	return _element[pos];
+// }
 
 
-
-// Allocating Memory using () initializes the value to 0
-/*
-    int*	i = new int();			// Default-initialized to 0
-    float*	f = new float();		// Default-initialized to 0.0
-    double*	d = new double();		// Default-initialized to 0.0
-    char*	c = new char();			// Default-initialized to '\0'
-
-    int*	i_uninit = new int;		// Uninitialized (garbage value)
-    float*	f_uninit = new float;	// Uninitialized (garbage value)
-    double*	d_uninit = new double;	// Uninitialized (garbage value)
-    char*	c_uninit = new char;	// Uninitialized (garbage value)
+// template <typename T>
+// int Array<T>::size(void)
+// {
+// 	return _len;
+// }
 
 
-// std::string is a class type with constructors that handle initialization:
-
-    std::string* s1 = new std::string();  // Default-initialized to ""
-    std::string* s2 = new std::string;   // Also default-initialized to ""
-
-*/
