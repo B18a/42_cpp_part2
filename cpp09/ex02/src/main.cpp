9fecdcfdacaf55a687896f17a77fc64eadb21c6c
BiBa <andreasjehle89@web.de>
2025-02-13 11:29:13 +0100

    added roadmap for ex02

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -1,39 +1,5 @@
 
 
-// TO-DO
-
-// Parse Input - positive Integers only
-// Optional - Print Numbers
-
-// Sorting Part - must be done 2 times (vector and deque)
-// Start Time - get current time
-// Sorting Function
-// {
-	// Parameter : Data and Current BlockSize - Beginning is 1 and then it multiplies by two each iteration
-	// Check if number of elements in Block is less than 2 (no sorting necessary) - odd number off integers
-	// get start iterator and end iterator in Block
-	// sort to pairs (tempalte function) - parameter : start iterator, end iterator, blocksize
-		// newIterator = std::advance(start, 2 + blocksize)
-		// while newIterator != end iterator
-		// {
-		// get BlockOneStartIterator
-		// get BlockOneEndIterator
-		// get BlockTwoStartIterator
-		// get BlockTwoEndIterator
-
-		// check if BlockTwoStartIterator or BlockTwoEndIterator are within end iterator
-		// get last elements of each Block and compare sizes
-		// if element in BlockOne is bigger
-		// swap_ranges function
-
-		// }
-// }
-
-
-// Finish Time - get current time
-
-
-
 
 
 #include"../include/PmergeMe.hpp"
