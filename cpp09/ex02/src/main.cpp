e62d0a3f4fdfcb51d2dd85493a4e4517340728fc
BiBa <andreasjehle89@web.de>
2025-02-13 11:23:00 +0100

    start of cpp09 ex02

---
+++ cpp09/ex02/src/main.cpp
@@ -0,0 +1,67 @@
+
+
+// TO-DO
+
+// Parse Input - positive Integers only
+// Optional - Print Numbers
+
+// Sorting Part - must be done 2 times (vector and deque)
+// Start Time - get current time
+// Sorting Function
+// {
+	// Parameter : Data and Current BlockSize - Beginning is 1 and then it multiplies by two each iteration
+	// Check if number of elements in Block is less than 2 (no sorting necessary) - odd number off integers
+	// get start iterator and end iterator in Block
+	// sort to pairs (tempalte function) - parameter : start iterator, end iterator, blocksize
+		// newIterator = std::advance(start, 2 + blocksize)
+		// while newIterator != end iterator
+		// {
+		// get BlockOneStartIterator
+		// get BlockOneEndIterator
+		// get BlockTwoStartIterator
+		// get BlockTwoEndIterator
+
+		// check if BlockTwoStartIterator or BlockTwoEndIterator are within end iterator
+		// get last elements of each Block and compare sizes
+		// if element in BlockOne is bigger
+		// swap_ranges function
+
+		// }
+// }
+
+
+// Finish Time - get current time
+
+
+
+
+
+#include"../include/PmergeMe.hpp"
+
+#include <vector>
+#include <deque>
+
+/****************************************/
+/*			Text Colors 				*/
+/****************************************/
+
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define RESET   "\033[0m" 
+
+
+
+int main(void)
+{
+	int arr[] = {5,3,7,9,6,4};
+
+
+
+
+}
+
